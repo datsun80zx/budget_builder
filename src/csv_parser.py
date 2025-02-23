9c38f5af2c33092e1ffaf70b3db755752761b0b8
datsun80zx <m.richardp93@gmail.com>
2025-02-23 10:44:19 -0500

    refactor

--- src/csv_parser.py
+++ src/csv_parser.py
@@ -2,13 +2,14 @@ import csv
 from budget_obj import CSVNode
 from pathlib import Path
 
-def node_create(file_name):
+def path_create(file_name: str):
     current_dir = Path.cwd()
-    csv_file = current_dir / "content" / file_name
+    return current_dir / "content" / file_name
 
+def node_create(file_path):
     csv_nodes = []
 
-    with open(csv_file, "r") as file:
+    with open(file_path, "r") as file:
         csv_reader = csv.reader(file)
         next(csv_reader)
         for line in csv_reader:
--- src/main.py
+++ src/main.py
@@ -1,9 +1,12 @@
-from csv_parser import node_create
+from csv_parser import node_create, path_create
 def main():
     file = "bank.csv"
-    list = node_create(file)
+    path = path_create(file)
+    list = node_create(path)
 
-    print(f"{list[0]}\n{list[1]}")
+    # print(f"{list[0]}\n{list[1]}")
+    for node in list:
+        print(node)
 
 
 if __name__ == "__main__":
