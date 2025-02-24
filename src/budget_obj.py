ea167a0d7df3047400640efe466865b0beb6d7de
datsun80zx <m.richardp93@gmail.com>
2025-02-24 08:03:37 -0500

    added the first version of the name creation function

--- src/budget_obj.py
+++ src/budget_obj.py
@@ -27,7 +27,45 @@ class CSVNode:
         self._create_node(line)
 
     def _get_name(self):
-        pass
+        desc = self.__desc__.strip()
+
+        if desc.startswith("ATM WITHDRAWAL"):
+            return "ATM"
+        
+        # split off the PPD ID and cleans up the whitespace before and after if any. 
+        desc = desc.split("PPD ID:")[0].strip()
+    
+
+        desc = ' '.join(part for part in desc.split() if not part.startswith(('01/', '02/', '03/', '04/', '05/', '06/', '07/', '08/', '09/', '10/', '11/', '12/')))
+        # print(desc)
+
+        # create a list of common state abbrev. to split off, 
+        states = ['AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'FL', 'GA', 'HI', 'ID', 'IL', 'IN', 
+              'IA', 'KS', 'KY', 'LA', 'ME', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 
+              'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 
+              'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WI', 'WY']
+        
+        words = desc.split()
+
+        cutoff_idx = len(words)
+        for i, word in enumerate(words):
+            if word in states:
+                cutoff_idx = i - 1
+                break
+
+        name = ' '.join(words[:cutoff_idx])
+        # print(name)
+
+        # remove some common suffixes
+        sfx_to_remove = ["CLUB FEES", "INS.PREM"]
+        for suffix in sfx_to_remove: 
+            if suffix in name.upper():
+                name = name.replace(suffix, '').strip()
+        
+        print(name.strip().title())
+
+
+
 
     def _create_node(self, csv_line):
         self.__type__ = csv_line[0]
--- src/main.py
+++ src/main.py
@@ -4,9 +4,15 @@ def main():
     path = path_create(file)
     list = node_create(path)
 
-    # print(f"{list[0]}\n{list[1]}")
-    for node in list:
-        print(node)
+    print(f"{list[0]}\n{list[1]}\n{list[2]}\n{list[3]}\n{list[4]}")
+    # for node in list:
+    #     node._get_name()
+
+    list[0]._get_name()
+    list[1]._get_name()
+    list[2]._get_name()
+    list[3]._get_name()
+    list[4]._get_name()
 
 
 if __name__ == "__main__":
